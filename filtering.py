803d879dbd3dad2cfda10193ee683392dc8c989f
Lobel Marunić <146864634+Mramar-croatia@users.noreply.github.com>
2025-01-21 15:24:30 +0100

    a
    a

--- filtering.py
+++ filtering.py
@@ -6,9 +6,10 @@ def remove_dates_for_person(tuples_list, cutoff):
         if datetime.strptime(date, "%d.%m.%Y.") <= cutoff
     ]
 
-def remove_non_wanted_dates(year_df):
+def remove_non_wanted_dates(year_df, cutoff_date):
     
-    cutoff_date = "25.12.2023."
+    # "25.12.2023."
+    
     cutoff_date_obj = datetime.strptime(cutoff_date, "%d.%m.%Y.")
     year_df['volunteer_dates'] = year_df['volunteer_dates'].apply(remove_dates_for_person, cutoff=cutoff_date_obj)
     
